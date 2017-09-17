a158173bcfee93c09958d373d8e0cfdfe64a0f6b
maicius <maicius@outlook.com>
2017-09-17 12:44:14 +0800

    完成对点赞和评论的人的统计

--- QQZone/QQZoneShuoshuo.py
+++ QQZone/QQZoneShuoshuo.py
@@ -14,7 +14,7 @@ class Spider(object):
         self.web = webdriver.Chrome()
         self.web.get('https://user.qzone.qq.com')
         self.__username = '1272082503'
-        self.__password = ''
+        self.__password = 'yuanhao110110HAO'
         self.headers = {
             'host': 'h5.qzone.qq.com',
             'accept-encoding': 'gzip, deflate, br',
@@ -116,8 +116,9 @@ class Spider(object):
     def get_mood_detail(self):
         urlMood = self.get_mood_url()
         url_Mood = urlMood + '&uin=' + str(1272082503)
+        self.re = connect_redis()
         pos = 0
-        while pos < 2000:
+        while pos < 1700:
             url__ = url_Mood + '&pos=' + str(pos)
             mood_detail = self.req.get(url=url__, headers=self.headers)
             jsonContent = self.get_json(str(mood_detail.content.decode('utf-8')))
@@ -128,27 +129,35 @@ class Spider(object):
                 # print('unikey' + unikey)
                 self.unikey = unikey
                 like_detail = self.get_like_list()
+
                 self.like_list_names.append(like_detail)
             # print('1272082503', jsonContent)
             # 存储到json文件
-            # with open('data' + str(pos) + '.json', 'w', encoding='utf-8') as w:
-            #     w.write(jsonContent)
+            with open('data' + str(pos) + '.json', 'w', encoding='utf-8') as w:
+                w.write(jsonContent)
             pos += 20
             print(pos)
         # time.sleep(2)
 
         # print(self.content)
-        self.re = connect_redis()
-        self.re.set("QQ", self.content)
-        self.re.set("QQ_like_list", self.like_list_names)
+        with open('agree' + '.json', 'w', encoding='utf-8') as w2:
+            json.dump(self.like_list_names, w2, ensure_ascii=False)
+
+        print(self.content)
+        print(json.dumps(self.content))
+        print("=====================")
+        print(json.dumps(self.like_list_names, ensure_ascii=False))
+        self.re.set("QQ", json.dumps(self.content, ensure_ascii=False))
+        self.re.set("QQ_like_list_all", json.dumps(self.like_list_names, ensure_ascii=False))
         print("finish")
 
     def get_like_list(self):
         url = self.get_aggree_url()
-        print(url)
+        # print(url)
         like_list = self.req.get(url=url, headers=self.headers)
         like_list_detail = self.get_json(like_list.content.decode('utf-8'))
-        print("success to get like list")
+        like_list_detail = like_list_detail.replace('\\n', '')
+        # print("success to get like list")
         return like_list_detail
 
 
@@ -164,7 +173,7 @@ class Spider(object):
         allunikey = []
         jsonData = json.loads(mood_detail)
         for item in jsonData['msglist']:
-            print(item.keys())
+            # print(item.keys())
             if 'pic' in item:
                 itemKey = item['pic']
                 if 'curlikekey' in itemKey[0]:
@@ -179,13 +188,14 @@ def connect_redis():
     re = redis.Redis(connection_pool=pool)
     return re
 
-def doAnalysis(fileName):
+def doAnalysis(fileName, commentNumber, commentList):
     # re = connect_redis()
     # data = re.get("QQ")
     # data = data.decode('utf-8').replace('\\', '')
     # print(data)
     f = open(fileName, encoding='utf-8')
     data = json.load(f)
+    agreeDict = []
     for item in data['msglist']:
         print(item['content'])
         # print(item.keys())
@@ -194,27 +204,64 @@ def doAnalysis(fileName):
         dt = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
         print(dt)
         # print(item['pic'])
+
         if 'pic' in item:
             itemKey = item['pic']
             if 'curlikekey' in itemKey[0]:
                 unikey = itemKey[0]['curlikekey'].split('^||^')
-                print(unikey)
+                # print(unikey)
         if 'commentlist' in item:
+            commentNumber.append(len(item['commentlist']))
             for item2 in item['commentlist']:
-                print(item2['name'])
+                if item2['name'] in commentList:
+                    commentList[item2['name']] +=1
+                else:
+                    commentList[item2['name']] = 1
+                # print(item2['name'])
+        else:
+            commentNumber.append(0)
+
+
     f.close()
 
 def getFileName():
+    commentList = {}
+    commentNumber = []
     pos = 0
-    while pos < 2000:
+    while pos < 1700:
         fileName = 'data' + str(pos) + '.json'
-        doAnalysis(fileName)
+        doAnalysis(fileName, commentNumber, commentList)
         pos += 20
+    f = open('agree.json', encoding='utf-8')
+    data = json.load(f)
+    totalAgree = 0
+    agreeNick = {}
+    agreeNumberList = []
+    for item in data:
+        item = json.loads(item)
+        uin_data = item['data']
+        totalAgree += int(uin_data['total_number'])
+        agreeNumberList.append(uin_data['total_number'])
+        # print(str(agreeNumberList))
+        for item_uin in uin_data['like_uin_info']:
+            if item_uin['nick'] in agreeNick:
+                agreeNick[item_uin['nick']] += 1
+            else:
+                agreeNick[item_uin['nick']] = 1
+    agreeNumberList.sort(reverse=True)
+    commentNumber.sort(reverse=True)
+    print("累计点赞数：" + str(totalAgree))
+    print("平均点赞数" + str(totalAgree / 1700))
+    print("点赞次数" + str(agreeNumberList))
+    print("点赞的人：" + str(sorted(agreeNick.items(), key= lambda item2: item2[1], reverse=True)))
+    # print("评论数:" + str(commentNumber))
+    print("给我评论的人：" + str(sorted(commentList.items(), key=lambda nameItem: nameItem[1], reverse=True)))
     print("finish")
+    f.close()
 
 if __name__ == '__main__':
-    sp = Spider()
-    sp.login()
-    print("Login success")
-    sp.get_mood_detail()
-    # getFileName()
+    # sp = Spider()
+    # sp.login()
+    # print("Login success")
+    # sp.get_mood_detail()
+    getFileName()
